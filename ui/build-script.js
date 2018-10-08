432a599257d1886cd1d064914fe11d633b00fd88
kahboom <rachelyordan@gmail.com>
2018-10-08 13:30:03 +0100

    Add test build script for bundling Angular output files

---
+++ ui/build-script.js
@@ -0,0 +1,15 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+
+(async function build() {
+  const files = [
+    './dist/inline.bundle.js',
+    './dist/polyfills.bundle.js',
+    './dist/main.bundle.js'
+  ];
+
+  await fs.ensureDir('elements');
+  await concat(files, 'elements/data-mapper.js');
+
+})();
+
